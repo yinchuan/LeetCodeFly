af651cdadcb70b6361776436b4f4eca6c15c6304
yinchuan <yinchuan.blue@gmail.com>
2023-12-03 15:16:58 +1100

    add ListNode definition

--- lib/ListNode.h
+++ lib/ListNode.h
@@ -5,4 +5,26 @@
 #ifndef LEETCODEFLY_LISTNODE_H
 #define LEETCODEFLY_LISTNODE_H
 
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+
+    void print() const {
+        cout << val << ", ";
+        auto cursor = next;
+        while(cursor != nullptr) {
+            cout << cursor->val << ", ";
+            cursor = cursor->next;
+        }
+        cout << endl;
+    }
+};
 #endif //LEETCODEFLY_LISTNODE_H
