059c5727bc4305f31e42939a388cb259a9ea8c57
yinchuan <yinchuan.blue@gmail.com>
2023-12-03 15:17:09 +1100

    add tools to print vector

--- lib/utils.h
+++ lib/utils.h
@@ -5,4 +5,19 @@
 #ifndef LEETCODEFLY_UTILS_H
 #define LEETCODEFLY_UTILS_H
 
+using namespace std;
+
+void print1D(const vector<int> &v) {
+    for (const auto entry: v) {
+        cout << entry << ", ";
+    }
+    cout << endl;
+}
+
+void print2D(const vector<vector<int>> &twoD) {
+    for(const auto &v: twoD) {
+        print1D(v);
+    }
+}
+
 #endif //LEETCODEFLY_UTILS_H
