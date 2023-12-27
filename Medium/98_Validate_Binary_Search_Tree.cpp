504a51a1a7f18c8af8ed5c09ff9f3cfdf251192c
yinchuan <yinchuan.blue@gmail.com>
2023-12-27 21:08:01 +1100

    use inorder traversal to validate binary search tree

--- Medium/98_Validate_Binary_Search_Tree.cpp
+++ Medium/98_Validate_Binary_Search_Tree.cpp
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/validate-binary-search-tree/
+// https://leetcode.com/problems/validate-binary-search-tree/description/
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -9,9 +9,11 @@
 #include <stack>
 #include <numeric>
 #include <queue>
+#include <cmath>
 #include "../lib/ListNode.h"
 #include "../lib/TreeNode.h"
 #include "../lib/utils.h"
+
 using namespace std;
 //#####
 /**
@@ -26,60 +28,76 @@ using namespace std;
  * };
  */
 class Solution {
-public:
-    bool isValidBST(TreeNode* root) {
-        ios_base::sync_with_stdio(false);
-
-        int maxLeft=INT32_MIN, minRight = INT32_MAX;
-
-        return recursion(root, maxLeft, minRight);
-    }
-
-    bool recursion(TreeNode* root, int &maxLeft, int &minRight) {
-        if (root == nullptr) return true;
-
-        if (root->left == nullptr && root->right == nullptr) {
-            maxLeft = max(maxLeft, root->val);
-            minRight = min(minRight, root->val);
-            return true;
+private:
+    bool inorder(TreeNode *root, TreeNode *&currentMax) {
+        if (root->left != nullptr && !inorder(root->left, currentMax)) {
+            return false;
         }
-        if (root->left != nullptr && root->val <= root->left->val) return false;
-        if (root->right != nullptr && root->val >= root->right->val) return false;
-
-        if (root->left != nullptr) {
-            int l = INT32_MIN;
-            int r = INT32_MAX;
-            if (!recursion(root->left, l, r) || root->val <= l) {
-                return false;
-            }
-            maxLeft = max(l, maxLeft);
-            minRight = min(r, minRight);
+        if (currentMax != nullptr && root->val <= currentMax->val) {
+            return false;
         }
-
-        if (root->right != nullptr) {
-            int l2 = INT32_MIN;
-            int r2 = INT32_MAX;
-            if (!recursion(root->right, l2, r2) || root->val >= r2) {
-                return false;
-            }
-            maxLeft = max(l2, maxLeft);
-            minRight = min(r2, minRight);
+        currentMax = root;
+        if (root->right != nullptr && !inorder(root->right, currentMax)) {
+            return false;
         }
 
         return true;
     }
 
+    bool iterInorder(TreeNode *root) {
+        stack<TreeNode *> s;
+        TreeNode *currentMax = nullptr;
+        auto node = root;
+        while (!s.empty() || node != nullptr) {
+            if (node != nullptr) {
+                s.push(node);
+                node = node->left;
+                continue;
+            }
+
+            node = s.top();
+            s.pop();
+            if (currentMax != nullptr && node->val <= currentMax->val) {
+                return false;
+            }
+            currentMax = node;
+            node = node->right;
+        }
+
+        return true;
+    }
+
+public:
+    bool isValidBST(TreeNode *root) {
+        ios_base::sync_with_stdio(false);
+
+        // store pointer of global max
+        // ususally use set initial value of currentMax to be INT32_MIN
+        // but a node could have a value of INT32_MIN, it's better to store the global max node
+        // learned from fast sample code
+
+
+        // recursive inorder
+        TreeNode *currentMax = nullptr;
+        return inorder(root, currentMax);
+
+        // return iterInorder(root);
+
+    }
 };
+
 //#####
 
 // do not copy lines after this to LeetCode
 int main() {
     Solution sol;
     bool ans;
-    TreeNode* root;
+    TreeNode *root;
+    TreeNode *&currentMax;
 
-    root;
-    ans = sol.isValidBST(root);
+    *root =;
+    &currentMax;
+    ans = sol.inorder(*root, &currentMax);
     std::cout << ans << std::endl;
 }
         
